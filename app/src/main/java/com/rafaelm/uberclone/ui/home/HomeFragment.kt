b2e39c9beb506266ec52d08610631cb426acb624
RafaelM <rafaelm.science@gmail.com>
2020-07-23 11:54:00 -0400

    Feat: Driver Location System - Correcao de bugs

--- app/src/main/java/com/rafaelm/uberclone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rafaelm/uberclone/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.rafaelm.uberclone.ui.home
 
+import android.annotation.SuppressLint
 import android.content.res.Resources
 import android.os.Bundle
 import android.os.Looper
@@ -18,7 +19,6 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.gms.maps.model.MarkerOptions
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -26,7 +26,6 @@ import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
 import com.rafaelm.uberclone.R
-import java.util.jar.Manifest
 
 class HomeFragment : Fragment(), OnMapReadyCallback {
 
@@ -62,6 +61,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         return root
     }
 
+    @SuppressLint("MissingPermission")
     private fun init() {
         locationRequest = LocationRequest()
         with(locationRequest) {
@@ -96,10 +96,11 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap?) {
         mMap = googleMap!!
 
-        //request permission
+        //Request Permission
         Dexter.withContext(requireContext())
             .withPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
             .withListener(object : PermissionListener {
+                @SuppressLint("MissingPermission")
                 override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
 
                     //Enable button first
@@ -109,29 +110,27 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
                     }
 
                     mMap.setOnMyLocationClickListener {
+
                         fusedLocationProviderClient.lastLocation
                             .addOnFailureListener { e ->
                                 Toast.makeText(context!!, e.message, Toast.LENGTH_SHORT).show()
-
                             }.addOnSuccessListener { location ->
-                                val userLatLtng = LatLng(location.latitude, location.longitude)
+                                val userLatLng = LatLng(location.latitude, location.longitude)
                                 mMap.animateCamera(
                                     CameraUpdateFactory.newLatLngZoom(
-                                        userLatLtng,
+                                        userLatLng,
                                         18f
                                     )
                                 )
                             }
                         true
+
                     }
-
-                    //layout
-
+                    //Layout
                     val locationButton = (mapFragment.requireView()
-                        .findViewById<View>("1".toInt())!!
-                        .parent!! as View).findViewById<View>("2".toInt())
+                        .findViewById<View>("1".toInt())!!.parent!! as View)
+                        .findViewById<View>("2".toInt())
                     val params = locationButton.layoutParams as RelativeLayout.LayoutParams
-
                     params.addRule(RelativeLayout.ALIGN_TOP, 0)
                     params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
                     params.bottomMargin = 50
@@ -147,12 +146,11 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
                 override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
                     Toast.makeText(
                         context!!,
-                        "Permission" + p0!!.permissionName + " was denied",
+                        "Permission" + p0!!.permissionName + "was denied",
                         Toast.LENGTH_SHORT
                     ).show()
                 }
-
-            })
+            }).check()
 
 
         try {
@@ -162,13 +160,10 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
                     R.raw.uber_maps_style
                 )
             )
-
             if (!success)
                 Log.e("UBER_ERROR", "Style parsing error")
         } catch (e: Resources.NotFoundException) {
-            Log.e("UBER_ERROR", e.message)
+            Log.e("UBER_ERROR", "" + e.message)
         }
-
-
     }
 }
